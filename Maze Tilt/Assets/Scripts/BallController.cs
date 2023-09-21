9350c22ec290d1b0cee39f1b3c2ed54bf831a0d4
Alekhya Ravi <alekhyar@usc.edu>
2023-09-20 18:27:19 -0700

    Maze with Tilt

---
+++ Maze Tilt/Assets/Scripts/BallController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public float moveForce = 10.0f;
+
+    private Rigidbody mRigidBody;
+
+    void Start()
+    {
+        mRigidBody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (mRigidBody != null)
+        {
+            float horizontalInput = Input.GetAxis("Horizontal");
+
+            Vector3 moveDirection = new Vector3(horizontalInput, 0, 0);
+
+            mRigidBody.AddForce(moveDirection * moveForce);
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                mRigidBody.AddForce(Vector3.up * 200);
+            }
+        }
+    }
+}
